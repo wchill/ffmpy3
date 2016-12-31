9901c6bc7d2be7a532d0d9f0a780a9199c9be35f
Eric Ahn <wchill1337@gmail.com>
2016-12-30 17:36:58 -0800

    Update README and package metadata

--- setup.py
+++ setup.py
@@ -1,32 +1,28 @@
 from setuptools import setup
 from setuptools.command.test import test as TestCommand  # noqa
-from ffmpy import __version__
+from ffmpy3 import __version__
 
 
 setup(
-    name='ffmpy',
+    name='ffmpy3',
     version=__version__,
-    description='A simple Python wrapper for ffmpeg',
+    description='A simple asynchronous Python wrapper for ffmpeg',
     long_description=open('README.rst').read(),
-    author='Andriy Yurchuk',
-    author_email='ay@mntw.re',
+    author='Eric Ahn',
+    author_email='ericahn3@illinois.edu',
     license='MIT',
-    url='https://github.com/Ch00k/ffmpy',
-    py_modules=['ffmpy'],
+    url='https://github.com/wchill/ffmpy3',
+    py_modules=['ffmpy3'],
     classifiers=[
         'Topic :: Multimedia :: Sound/Audio',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
         'Operating System :: POSIX :: BSD',
